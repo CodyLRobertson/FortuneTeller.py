4fd19bc4822730bca9fcf866384af3453aa5c2aa
Cody L. Robertson <unlinkedcody@yahoo.com>
2019-07-21 02:09:35 -0500

    Continued Development of Main function.
    Need to fix random answer generation and printing.

--- FortuneTeller.py
+++ FortuneTeller.py
@@ -19,15 +19,36 @@ def getUsername():
 def getUserQuestion():
 	userQuestion = input("What is your question?")
 	print("You have asked: " + userQuestion)
-	continueGame = input(str.lower("Is this correct? Y/N"))
-	if continueGame == "y":
-		break
-	else:
-		getUserQuestion()
-
 #4 		- Main Program
-#4.2 	- Collect random int 
+def getAnswer(answerNumber):
+	if answerNumber == 1:
+		return 'It is certain'
+	elif answerNumber == 2:
+		return 'It is decidely so.'
+	elif answerNumber == 3:
+		return 'Yes.'
+	elif answerNumber == 4:
+		return 'Reply hazy, try again.'
+	elif answerNumber == 5:
+		return 'Ask again later.'
+	elif answerNumber == 6:
+		return 'Concentrate and ask again.'
+	elif answerNumber == 7:
+		return 'My replay is no.'
+	elif answerNumber == 8:
+		return 'Outlook not so good.'
+	elif answerNumber == 9:
+		return 'Very doubtful.'
+	elif answerNumber == 10:
+		return 'Hard No.'
+r = random.randint(1,10)
+answer = getAnswer(r)
+print(answer)
+
+#4.2 	- Collect random int
 #4.3 	- Display Answer based on int
+
+
 #4.4 	- Display original Question
 #5		- Ask to repeat? Y/N
 #6 		- If (N) End program gracefully
@@ -35,6 +56,8 @@ def getUserQuestion():
 def Main():
 	getTimeAndDate()
 	getUsername()
+	getUserQuestion()
+	getAnswer()
 
 
 Main()
